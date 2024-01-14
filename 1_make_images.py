af82eaddb4bb52d7ab60ce143d6c51a3fca0c6c7
Miron Bodewes <Miron.bodewes@gmail.com>
2024-01-14 16:33:43 +0100

    .

--- 1_make_images.py
+++ 1_make_images.py
@@ -11,25 +11,27 @@ from glob import glob
 from os import path
 
 SPACE_KEY_PRESS = 32
-DIRECTORY="./data/"
+VIDEO_CAM = 2
+DIRECTORY = "./data/"
 if os.path.exists(DIRECTORY):
     print("moving old chessboards to another folder")
-    _datetime=datetime.datetime.now()
-    shutil.move("./data/","./databefore"+str(_datetime)+"/")
+    _datetime = datetime.datetime.now()
+    shutil.move("./data/", "./databefore"+str(_datetime)+"/")
 # os.mkdir(DIRECTORY)
-i=100
+i = 100
 cv2.namedWindow("Camera Calibration", cv2.WINDOW_NORMAL)
-cam = cv2.VideoCapture(2)
+cam = cv2.VideoCapture(VIDEO_CAM)
 while True:
     ret, img_orig = cam.read()
     assert ret, 'Camera read failed'
     img = img_orig.copy()
-    img = cv2.putText(img, '<SPACE> to make images, <s> to stop', tuple(x//2 for x in img_orig.shape[:2]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
+    img = cv2.putText(img, '<SPACE> to make images, <s> to stop', tuple(
+        x//7 for x in img_orig.shape[:2]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
     cv2.imshow('Camera Calibration', img)
     key = cv2.waitKey(1) & 0xFF
     if key == SPACE_KEY_PRESS:
-        meep=cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
-        i+=1
-        cv2.imwrite("./data/checkerboard_"+str(i)+".png",meep)
+        meep = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+        i += 1
+        cv2.imwrite("./data/checkerboard_"+str(i)+".png", meep)
     elif key == ord("s"):
         break
